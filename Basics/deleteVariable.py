2ed71bfff300b722c321f9365074e3693ec3dc04
gowthamReddy <gowthamreddy.6991@gmail.com>
2023-08-28 21:30:47 +0530

    basics

---
+++ Basics/deleteVariable.py
@@ -0,0 +1,7 @@
+a=10
+print("before delete the variable")
+print(a)
+
+del a
+print("after delete the variable")
+print(a)    # this will throw an error because of the variable value will be delete, there is no variable
---
+++ Basics/swapingTheVariables.py
@@ -0,0 +1,8 @@
+a=10
+b=20
+print("before swaping")
+print(a,b)
+
+a,b=b,a
+print("after swaping")
+print(a,b)
