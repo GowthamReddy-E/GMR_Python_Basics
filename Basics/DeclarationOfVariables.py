f496c7e8b8ce39ef7912006eda7d39205e507800
gowthamReddy <gowthamreddy.6991@gmail.com>
2023-08-28 21:19:50 +0530

    basics

---
+++ Basics/DeclarationOfVariables.py
@@ -0,0 +1,25 @@
+a=10
+b=20
+c=30
+
+print(a,b,c)
+
+d,e,f=10,20,30
+
+print(d,e,f)
+
+g=h=i=30
+
+print(i)
+
+# number declaration
+number=10
+print(f"number: {number}")
+
+# string declaration
+string="gowtham"
+print(f"string: {string}")
+
+# boolean declaration
+boolean=True
+print(f"boolean: {boolean}")
---
+++ Basics/comments.py
@@ -0,0 +1,20 @@
+# single line comments 
+
+print ("single line comment")
+
+"""
+multi line comment
+multi line comment
+multi line comment
+"""
+
+print('multi line comments')
+
+
+'''  
+multi line comment
+multi line comment
+multi line comment 
+'''
+
+print('multi line comments')
