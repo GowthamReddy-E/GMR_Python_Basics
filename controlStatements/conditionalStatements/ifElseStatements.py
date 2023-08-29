7f60806030c5e708fad2e495421d106e2c5b4f2a
gowthamReddy <gowthamreddy.6991@gmail.com>
2023-08-29 11:02:27 +0530

    loops and conditional statements

---
+++ controlStatements/conditionalStatements/ifElseStatements.py
@@ -0,0 +1,6 @@
+# if else statements
+name="sharmika"
+if  name=="gowtham":
+    print(f"{name} is good boy")
+else:
+    print(f"{name} is not a good boy")
---
+++ controlStatements/loopingStatements/forLoop.py
@@ -0,0 +1,18 @@
+# sample loop to print the values 1 to 10
+
+for i in range(11):
+    print(i)
+print("loop is completed")
+
+
+# to print even numbers only
+for i in range(0,11,2):
+    print(i)
+
+# to print reverse order
+for i in range(10,0,-1):
+    print(i)
+
+# to print reverse order negative values
+for i in range(-10,0,1):
+    print(i)
