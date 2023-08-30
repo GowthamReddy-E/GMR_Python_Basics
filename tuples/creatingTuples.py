6c1072544472b28f174ff566ba3422bdc0bca24c
gowthamReddy <gowthamreddy.6991@gmail.com>
2023-08-30 18:15:48 +0530

    tuples

---
+++ tuples/creatingTuples.py
@@ -0,0 +1,12 @@
+t1=( )  # empty tuple)
+
+t2 =([1,2,3,4,5,6,7,8,9])
+
+t3=(1,2,3,4,5,6,7,8,9,10)
+
+t4 =tuple("gowtham")
+
+print(t1)
+print(t2)
+print(t3)
+print(t4)
---
+++ tuples/inAndNotIn.py
@@ -0,0 +1,7 @@
+t=(1,2,3,4,5,6,7,8,9,10)
+
+# in 
+print(12 in t)
+
+# not in
+print(12 not in t)
