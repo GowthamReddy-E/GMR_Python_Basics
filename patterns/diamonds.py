42e6dc0126a74fcd37502210f67e956bb4c4b011
gowthamReddy <gowthamreddy.6991@gmail.com>
2023-09-04 20:58:55 +0530

    string operations

--- patterns/diamonds.py
+++ patterns/diamonds.py
@@ -1,42 +1,42 @@
-# print("\t\t*")
-# print("\t*\t\t*")
-# print("\t\t*")
+print("\t\t*")
+print("\t*\t\t*")
+print("\t\t*")
 
 
 
-# print("\t\t\t*")
-# print("\t\t*   *   *")
-# print("\t*   *   *   *   *")
-# print("\t\t*   *   *")
-# print("\t\t\t*")
+print("\t\t\t*")
+print("\t\t*   *   *")
+print("\t*   *   *   *   *")
+print("\t\t*   *   *")
+print("\t\t\t*")
 
 
 
 
-# print("\t\t\t\t\t*")
-# print("\t\t\t\t*   *   *")
-# print("\t\t\t*   *   *   *   *")
-# print("\t\t*   *   *   *   *   *   *")
-# print("\t*   *   *   *   *   *   *   *   *")
-# print("\t\t*   *   *   *   *   *   *")
-# print("\t\t\t*   *   *   *   *")
-# print("\t\t\t\t*   *   *")
-# print("\t\t\t\t\t*")
+print("\t\t\t\t\t*")
+print("\t\t\t\t*   *   *")
+print("\t\t\t*   *   *   *   *")
+print("\t\t*   *   *   *   *   *   *")
+print("\t*   *   *   *   *   *   *   *   *")
+print("\t\t*   *   *   *   *   *   *")
+print("\t\t\t*   *   *   *   *")
+print("\t\t\t\t*   *   *")
+print("\t\t\t\t\t*")
 
 
-# print("\t\t\t\t\t*","\n\t\t\t\t*","*","*","*\n","\t\t\t*","*","*","*","*\n","\t\t*","*","*","*","*","*","*\n","\t*","*","*","*","*","*","*","*","*",sep="   ")
+print("\t\t\t\t\t*","\n\t\t\t\t*","*","*","*\n","\t\t\t*","*","*","*","*\n","\t\t*","*","*","*","*","*","*\n","\t*","*","*","*","*","*","*","*","*",sep="   ")
 
 
 
-# print("\t\t\t   *",end="\n")
-# print("\t\t\t   *",end="\n")
-# print("\t\t   *","*","*",sep="\t",end="\n")
-# print("\t   *","*","*","*","*",sep="\t",end="\n")
-# print("\t*","*","*","*","*","*","*",sep="\t",end="  \n")
-# print("\t   *","*","*","*","*",sep="\t",end="\n")
-# print("\t\t   *","*","*",sep="\t",end="\n")
-# print("\t\t\t   *",end="\n")
-# print("\t\t\t   *",end="\n")
+print("\t\t\t   *",end="\n")
+print("\t\t\t   *",end="\n")
+print("\t\t   *","*","*",sep="\t",end="\n")
+print("\t   *","*","*","*","*",sep="\t",end="\n")
+print("\t*","*","*","*","*","*","*",sep="\t",end="  \n")
+print("\t   *","*","*","*","*",sep="\t",end="\n")
+print("\t\t   *","*","*",sep="\t",end="\n")
+print("\t\t\t   *",end="\n")
+print("\t\t\t   *",end="\n")
 
 
 print("\t\t\t   *",end="\n")
